69c2f2eec3228ff7dd4b4fe2b81248f9075a080b
ArbajPathan <arbajpatel0432>
2023-08-09 15:54:43 +0530

    Done with GET back data fron crudcrud

--- user.js
+++ user.js
@@ -1,44 +1,63 @@
 
-        const Form=document.querySelector('#my-form');
-        const Username=document.querySelector('#name');
-        const UserId=document.querySelector('#email');
-        const Userphone=document.querySelector('#phone');
-        const list=document.querySelector('#listofuser');
-      
-        Form.addEventListener('submit', onsubmit)
+const Form = document.querySelector('#my-form');
+const Username = document.querySelector('#name');
+const UserId = document.querySelector('#email');
+const Userphone = document.querySelector('#phone');
+const list = document.querySelector('#listofuser');
 
-        function onsubmit (e){
-            e.preventDefault();
-              
-            const delbtn=document.createElement('button');
-            delbtn.value=delete
-            delbtn.appendChild(document.createTextNode("Delet User"))
-            const editbtn=document.createElement('button');
-            editbtn.appendChild(document.createTextNode("Edit User"))
-            const li=document.createElement('li')
-            li.appendChild(document.createTextNode(`${Username.value} : ${UserId.value} : ${Userphone.value}`))
+Form.addEventListener('submit', onsubmit)
 
-            li.appendChild(delbtn)
-            li.appendChild(editbtn)
-            list.appendChild(li)
+function onsubmit(e) {
+    e.preventDefault();
 
-            const name=Username.value;
-            const email=UserId.value;
-            const phone=Userphone.value;
-            let obj={
-                name,
-                email,
-                phone
+    const delbtn = document.createElement('button');
+    delbtn.value = delete
+        delbtn.appendChild(document.createTextNode("Delete User"))
+    const editbtn = document.createElement('button');
+    editbtn.appendChild(document.createTextNode("Edit User"))
+    const li = document.createElement('li')
+    li.appendChild(document.createTextNode(`${Username.value} : ${UserId.value} : ${Userphone.value}`))
+
+    li.appendChild(delbtn)
+    li.appendChild(editbtn)
+    list.appendChild(li)
+
+    const name = Username.value;
+    const email = UserId.value;
+    const phone = Userphone.value;
+    let obj = {
+        name,
+        email,
+        phone
+    }
+    axios.post("https://crudcrud.com/api/c639deb76fbf4e10bd6db21ed1059d99/UserDetails", obj)
+        .then((res) => {
+            console.log(res.data)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+
+
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+    axios.get("https://crudcrud.com/api/c639deb76fbf4e10bd6db21ed1059d99/UserDetails")
+        .then((res) => {
+            for (let i = 0; i < res.data.length; i++) {
+                GETBack(res.data[i])
             }
-            axios.post("https://crudcrud.com/api/1a2e54474797488597fa3abb03a9e1db/UserDetails" , obj )
-            .then((res) => {
-             console.log (res)
-            })
-            .catch ((err) => {
-               console.log("<h4> Something Went Wrong </h4>")
-            })
-           
-        }
-        
+        })
+        .catch((err) => {
+            console.log(err)
+        })
 
-       
+});
+function GETBack(data) {
+    var parentNode = document.getElementById("listofuser")
+    var chilsHTML = `<li >${data.name} ${data.email} ${data.phone}
+    <button >Delete User</button>
+    <button >Edit User</button>
+     </li>`
+    parentNode.innerHTML = parentNode.innerHTML + chilsHTML
+}
